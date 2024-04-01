d12938fd1513dd8972119af18f28c76351523f13
prettycrunchyguy <137006173+prettycrunchyguy@users.noreply.github.com>
2024-04-01 14:29:58 -0500

    Create easy.js

---
+++ easy.js
@@ -0,0 +1,26 @@
+let crunchyi = 0;
+(function(){
+  function cmd(a){
+    document.getElementById('c-input').value = a;
+    document.getElementById('c-input').dispatchEvent(new KeyboardEvent('keyup', {
+      key: 'Enter',
+      code: 'Enter',
+      which: 13,
+      keyCode: 13,
+    }))
+  }
+  return setInterval(function(){
+    if (crunchyi % 5 == 0){
+      cmd('execute as @a run warp rooms');
+      setTimeout(function(){
+        cmd('execute at @p run spawnpoint @a');
+      },15000);
+    }else{
+      cmd('execute as @a run warp ' + Math.random());
+      setTimeout(function(){
+        cmd('execute at @p run spawnpoint @a');
+      },15000);
+    }
+    crunchyi++;
+  },120000);
+})()
