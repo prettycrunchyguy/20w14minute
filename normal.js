ffbe2f83aabcda115bea1cb443a7895e64b30e35
prettycrunchyguy <137006173+prettycrunchyguy@users.noreply.github.com>
2024-04-01 14:15:47 -0500

    Create normal.js

---
+++ normal.js
@@ -0,0 +1,17 @@
+(function(){
+  function cmd(a){
+    document.getElementById('c-input').value = a;
+    document.getElementById('c-input').dispatchEvent(new KeyboardEvent('keyup', {
+      key: 'Enter',
+      code: 'Enter',
+      which: 13,
+      keyCode: 13,
+    }))
+  }
+  return setInterval(function(){
+    cmd('execute as @a run warp ' + Math.random());
+    setTimeout(function(){
+      cmd('execute at @p run spawnpoint @a');
+    },15000);
+  },120000);
+})()
